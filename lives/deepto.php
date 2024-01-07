15dcca439777bf7452719180faff5e8698c4d200
bdiptv24 <154502197+bdiptv24@users.noreply.github.com>
2024-01-07 07:59:44 +0600

    deepto.php

--- lives/deepto.php
+++ lives/deepto.php
@@ -1,146 +1,142 @@
 <?php
-
-
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <title>STARPLAY LIVE SPORTS</title>
-    <meta name="robots" content="noindex,nofollow">
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <style>
-        body,
-        html {
-            margin: 0;
-            padding: 0;
-        }
-
-        .jwplayer {
-            position: absolute !important;
-        }
-
-        .jwplayer.jw-flag-aspect-mode {
-            min-height: 100%;
-            max-height: 100%;
-        }
-
-        .jw-logo-container {
-            position: absolute;
-            top: 2%;
-            right: 55%;
-        }
-    #logoContainer {
-            position: absolute;
-            top: 15px;
-            right: 10px;
-            z-index: 999;
-        }
-
-        #logoContainer img {
-            max-width: 80px;
-        }
-
-
-        @media (min-width: 600px) {
-            #myElement {
-                max-width: 620px;
-                max-height: 350px;
-                margin-top: 50px;
-                margin-left: 5%;
-                margin-right: auto;
-            }
-        }
-
-        @media (max-width: 600px) {
-            #myElement {
-                max-width: 320px;
-                max-height: 180px;
-                margin-top: 100px;
-                margin-left: 5%;
-                margin-right: auto;
-            }
-        }
-
-        #myElement {
-            box-shadow: 0 10px 50px rgba(148, 255, 149, 0.85), 5px 10px 50px rgba(0, 0, 0, 0.65);
-        }
-
-        p {
-            margin-bottom: 50px;
-            background-color: white;
-            margin-left: auto;
-            margin-right: auto;
-            display: table;
-            font-family: sans-serif;
-            font-size: 20px;
-            color: cornflowerblue;
-        }
-
-        #settingsContainer {
-            position: absolute;
-            bottom: 10px;
-            right: 10px;
-            z-index: 999;
-        }
-
-        #settingsContainer button {
-            padding: 5px 10px;
-            background-color: #007bff;
-            color: white;
-            border: none;
-            cursor: pointer;
-        }
-    </style>
-</head>
-
-<body>
-    <div id='logoContainer'>
-        <img src='https://imgtr.ee/images/2023/12/26/1ce485d929f0c4b3bdef1810f915a21d.png' alt='Logo'>
-    </div>
-    <div id='myElement'>Loading the player...</div>
-
-
-   <script type='text/javascript' src="//starplay2.netlify.app/player/freeiptv25_v2_jwplayercustom_jwplayer-7.11.0_jwplayer.js"></script>
-    <script type='text/javascript'>
-        jwplayer.key = "JTGE07Ok+RFyC39Hbk/0sU5pENovRRZIhEzX9Q==";
-    </script>
-    <script type='text/javascript'>
-        this.player = jwplayer("myElement");
-        this.player.setup({
-            "preload": "auto",
-            "hlshtml": true,
-            "autostart": true,
-            "primary": "html5",
-
-            "displaydescription": false,
-            "file": "https://byphdgllyk.gpcdn.net/DeeptoTV/deeptotv-manifest.m3u8",
-            "title": "STAR PLAY",
-            "displaytitle": true,
-            "type": "hls",
-            "id": "vemba_player_EvpUz",
-
-"logo": {
-                file: "",
-                hide: false,
-                position: "top-right",
-                height: "30px",
-                width: "40px",
-                        },
-            "controls": true,  // Enable default controls
-            "controlbar": {
-                "autoQualityButton": true  // Enable auto quality button
-            }
-        });
-
-        var plugin = this.player.getPlugin();
-        console.log('plugin:', plugin);
-    </script>
-</body>
-
-</html>
-
+echo '<!DOCTYPE html>';
+echo '<html lang="en">';
+echo '';
+echo '<head>';
+echo '<title>STARPLAY LIVE SPORTS</title>';
+echo '<meta name="robots" content="noindex,nofollow">';
+echo '<meta http-equiv="Content-Type" content="text/html; charset=utf-8">';
+echo '<meta http-equiv="X-UA-Compatible" content="IE=edge">';
+echo '<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">';
+echo '';
+echo '<style>';
+echo 'body,';
+echo 'html {';
+echo 'margin: 0;';
+echo 'padding: 0;';
+echo '}';
+echo '';
+echo '.jwplayer {';
+echo 'position: absolute !important;';
+echo '}';
+echo '';
+echo '.jwplayer.jw-flag-aspect-mode {';
+echo 'min-height: 100%;';
+echo 'max-height: 100%;';
+echo '}';
+echo '';
+echo '.jw-logo-container {';
+echo 'position: absolute;';
+echo 'top: 2%;';
+echo 'right: 55%;';
+echo '}';
+echo '#logoContainer {';
+echo 'position: absolute;';
+echo 'top: 15px;';
+echo 'right: 10px;';
+echo 'z-index: 999;';
+echo '}';
+echo '';
+echo '#logoContainer img {';
+echo 'max-width: 80px;';
+echo '}';
+echo '';
+echo '';
+echo '@media (min-width: 600px) {';
+echo '#myElement {';
+echo 'max-width: 620px;';
+echo 'max-height: 350px;';
+echo 'margin-top: 50px;';
+echo 'margin-left: 5%;';
+echo 'margin-right: auto;';
+echo '}';
+echo '}';
+echo '';
+echo '@media (max-width: 600px) {';
+echo '#myElement {';
+echo 'max-width: 320px;';
+echo 'max-height: 180px;';
+echo 'margin-top: 100px;';
+echo 'margin-left: 5%;';
+echo 'margin-right: auto;';
+echo '}';
+echo '}';
+echo '';
+echo '#myElement {';
+echo 'box-shadow: 0 10px 50px rgba(148, 255, 149, 0.85), 5px 10px 50px rgba(0, 0, 0, 0.65);';
+echo '}';
+echo '';
+echo 'p {';
+echo 'margin-bottom: 50px;';
+echo 'background-color: white;';
+echo 'margin-left: auto;';
+echo 'margin-right: auto;';
+echo 'display: table;';
+echo 'font-family: sans-serif;';
+echo 'font-size: 20px;';
+echo 'color: cornflowerblue;';
+echo '}';
+echo '';
+echo '#settingsContainer {';
+echo 'position: absolute;';
+echo 'bottom: 10px;';
+echo 'right: 10px;';
+echo 'z-index: 999;';
+echo '}';
+echo '';
+echo '#settingsContainer button {';
+echo 'padding: 5px 10px;';
+echo 'background-color: #007bff;';
+echo 'color: white;';
+echo 'border: none;';
+echo 'cursor: pointer;';
+echo '}';
+echo '</style>';
+echo '</head>';
+echo '';
+echo '<body>';
+echo '<div id='logoContainer'>';
+echo '<img src='https://imgtr.ee/images/2023/12/26/1ce485d929f0c4b3bdef1810f915a21d.png' alt='Logo'>';
+echo '</div>';
+echo '<div id='myElement'>Loading the player...</div>';
+echo '';
+echo '';
+echo '<script type='text/javascript' src="//starplay2.netlify.app/player/freeiptv25_v2_jwplayercustom_jwplayer-7.11.0_jwplayer.js"></script>';
+echo '<script type='text/javascript'>';
+echo 'jwplayer.key = "JTGE07Ok+RFyC39Hbk/0sU5pENovRRZIhEzX9Q==";';
+echo '</script>';
+echo '<script type='text/javascript'>';
+echo 'this.player = jwplayer("myElement");';
+echo 'this.player.setup({';
+echo '"preload": "auto",';
+echo '"hlshtml": true,';
+echo '"autostart": true,';
+echo '"primary": "html5",';
+echo '';
+echo '"displaydescription": false,';
+echo '"file": "https://byphdgllyk.gpcdn.net/DeeptoTV/deeptotv-manifest.m3u8",';
+echo '"title": "STAR PLAY",';
+echo '"displaytitle": true,';
+echo '"type": "hls",';
+echo '"id": "vemba_player_EvpUz",';
+echo '';
+echo '"logo": {';
+echo 'file: "",';
+echo 'hide: false,';
+echo 'position: "top-right",';
+echo 'height: "30px",';
+echo 'width: "40px",';
+echo '},';
+echo '"controls": true,  // Enable default controls';
+echo '"controlbar": {';
+echo '"autoQualityButton": true  // Enable auto quality button';
+echo '}';
+echo '});';
+echo '';
+echo 'var plugin = this.player.getPlugin();';
+echo 'console.log('plugin:', plugin);';
+echo '</script>';
+echo '</body>';
+echo '';
+echo '</html>';
 ?>
